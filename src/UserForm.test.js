fcd93bae3ef884c732dee4cc683fa307ad13635f
leobewater <mmbee888@gmail.com>
2024-02-27 18:17:11 -0500

    mock function

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -15,13 +15,14 @@ test("it shows two inputs and a button", () => {
   expect(button).toBeInTheDocument();
 });
 
+/*
+// NOT THE BEST IMPLEMENTATION
 test("it calls onUserAdd when the form is submitted", () => {
-  // NOT THE BEST IMPLEMENTATION
   const argList = [];
   const callback = (...args) => {
     argList.push(args);
   };
-
+  
   render(<UserForm onUserAdd={callback} />);
 
   const [nameInput, emailInput] = screen.getAllByRole("textbox");
@@ -41,4 +42,31 @@ test("it calls onUserAdd when the form is submitted", () => {
   // Assertion to make sure 'onUserAdd' gets called with email/name
   expect(argList).toHaveLength(1);
   expect(argList[0][0]).toEqual({ name: "jane", email: "jane@jane.com" });
+});
+*/
+
+
+// use mock function
+test("it calls onUserAdd when the form is submitted", () => {
+  const mock = jest.fn();
+
+  render(<UserForm onUserAdd={mock} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  // Simulate typing in a name
+  user.click(nameInput);
+  user.keyboard("jane");
+
+  // Simulate typing in an email
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  // Find the button
+  const button = screen.getByRole("button");
+  user.click(button);
+
+  // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@jane.com" });
 });
