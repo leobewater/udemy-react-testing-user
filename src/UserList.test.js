fbf5bc495febdcb1a6b4731b5a57faf53d644d09
leobewater <mmbee888@gmail.com>
2024-02-27 19:02:21 -0500

    use query selector to select table rows

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,6 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import { UserList } from './UserList';
+import { render, screen, within } from "@testing-library/react";
+import { UserList } from "./UserList";
 
-test('', () => {
+test("render one row per user", () => {
+  // Render the components
+  const users = [
+    { name: "jane", email: "jane@jane.com" },
+    { name: "sam", email: "sam@sam.com" },
+  ];
 
-})
+  const { container } = render(<UserList users={users} />);
+
+  // use the online ide to run test playround
+  // screen.logTestingPlaygroundURL();
+ 
+  // const table = container.querySelector("table");
+  // console.log(table);
+
+  // Find all the rows in the table by testid
+  // const rows = within(screen.getByTestId("users")).getAllByRole("row");
+
+  // Find all rows with querySelector
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll("tbody tr");
+
+  // Assertion: correct number of rows in the table
+  expect(rows).toHaveLength(2);
+});
+
+test("render the email and name of each user", () => {});
