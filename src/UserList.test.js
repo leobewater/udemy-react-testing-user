d173a7c0470a97244935006ebda1e20461d34e74
leobewater <mmbee888@gmail.com>
2024-02-28 09:17:07 -0500

    refactor

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,14 +1,22 @@
 import { render, screen, within } from "@testing-library/react";
 import { UserList } from "./UserList";
 
-test("render one row per user", () => {
-  // Render the components
+function renderComponent() {
   const users = [
     { name: "jane", email: "jane@jane.com" },
     { name: "sam", email: "sam@sam.com" },
   ];
 
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
+
+  return {
+    users,
+  };
+}
+
+test("render one row per user", () => {
+  // Render the components
+  renderComponent();
 
   // ****** use the online ide to run test playround ****************
   // screen.logTestingPlaygroundURL();
@@ -29,12 +37,7 @@ test("render one row per user", () => {
 
 test("render the email and name of each user", () => {
   // Render the components
-  const users = [
-    { name: "jane", email: "jane@jane.com" },
-    { name: "sam", email: "sam@sam.com" },
-  ];
-
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   // Find all the rows in the table by testid
   for (let user of users) {
